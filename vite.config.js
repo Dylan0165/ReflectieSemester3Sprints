40d465a8af09beed8301053651765b68775d71fb
Dylan Schuurman <dylan.schuurman@student.fontys.nl>
2025-10-19 00:48:18 +0200

    teset20

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Dit zorgt dat je assets correct geladen worden in containers (ook bij Nginx)
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative paths voor deployment
+  base: './', // belangrijk: relative paths
   build: {
     outDir: 'dist',
   },
