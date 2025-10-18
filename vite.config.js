e81017247f2e11c8a7ffb6f7bf34df87ea43e313
Dylan Schuurman <dylan.schuurman@student.fontys.nl>
2025-10-19 01:40:22 +0200

    test29

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ heel belangrijk: relatieve paden in CI-build
+  base: './',
   build: { outDir: 'dist' },
 })
